fddc0a9fe394c42c55874a4a42eed83ebe59f50b
keeprich <keeprich@keeprichs-air.lan>
2020-04-05 06:18:39 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    MyProfileComponent,
+    MyPortfolioComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule,
